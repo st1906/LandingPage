958a026fee6fcfec5166464374e9b25b8d85cc2a
Stipan Hržić <stipan.hrzic@gmail.com>
2019-04-28 15:48:09 +0200

    Image modal

--- app.js
+++ app.js
@@ -7,3 +7,22 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         });
     });
 });
+
+//Scale images up on click an place them center
+// Get the modal
+let modal = document.getElementById('myModal');
+let modalImg = document.getElementById("img01");
+const imageFeed = document.getElementById("photo-feed");
+const temp = imageFeed.querySelectorAll("img");
+const pictures = Array.prototype.slice.call(temp);
+function scaleUp(){
+    modal.style.display = "block";
+    modalImg.src = this.src;
+    // Get the <span> element that closes the modal
+    let span = document.getElementsByClassName("close")[0];
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() { 
+        modal.style.display = "none";
+    }
+}
+pictures.forEach((pic)=>pic.addEventListener("click", scaleUp));
