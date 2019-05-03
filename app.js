a8434140a17772165c13da3f4bbc8a0d6b905613
Stipan Hržić <stipan.hrzic@gmail.com>
2019-05-03 14:05:17 +0200

    Gallery fully implemented

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+//Scrolling animation of link clicks
 document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     anchor.addEventListener('click', function (e) {
         e.preventDefault();
@@ -8,16 +9,20 @@ document.querySelectorAll('a[href^="#"]').forEach(anchor => {
     });
 });
 
-//Scale images up on click an place them center
+
 // Get the modal
 let modal = document.getElementById('myModal');
 let modalImg = document.getElementById("img01");
 const imageFeed = document.getElementById("photo-feed");
 const temp = imageFeed.querySelectorAll("img");
 const pictures = Array.prototype.slice.call(temp);
-function scaleUp(){
+console.log(pictures);
+function popUp(){
     modal.style.display = "block";
     modalImg.src = this.src;
+    modalImg.dataset.reg = this.dataset.id;
+    let id = this.dataset.id;
+    console.log(id);
     // Get the <span> element that closes the modal
     let span = document.getElementsByClassName("close")[0];
     // When the user clicks on <span> (x), close the modal
@@ -25,4 +30,33 @@ function scaleUp(){
         modal.style.display = "none";
     }
 }
-pictures.forEach((pic)=>pic.addEventListener("click", scaleUp));
+pictures.forEach((pic)=>pic.addEventListener("click", popUp));
+
+
+let rightArrow = document.querySelector(".fa-arrow-right");
+let leftArrow = document.querySelector(".fa-arrow-left");
+
+function onArrowClickD(){
+    let modalF = this.parentNode.parentNode;
+    let image = modalF.querySelector("img");
+    let dataID = image.dataset.reg; 
+    let int = parseInt(dataID) + 1;
+    if(int === 10)
+        int = 1;
+    image.src = pictures[int - 1].src;
+    image.dataset.reg = int;
+
+}
+function onArrowClickL(){
+    let modalF = this.parentNode.parentNode;
+    let image = modalF.querySelector("img");
+    let dataID = image.dataset.reg; 
+    let int = parseInt(dataID) - 1;
+    if(int === 0)
+        int = 9;
+    image.src = pictures[int - 1].src;
+    image.dataset.reg = int;
+
+}
+rightArrow.addEventListener("click", onArrowClickD);
+leftArrow.addEventListener("click", onArrowClickL);
